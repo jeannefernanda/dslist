7736ccce701881b5ce418161015cac88ead8ab5e
Jeanne Fernanda <jeannefernanda.m@gmail.com>
2023-07-15 17:43:49 -0400

    Domain model

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+	
+	@EmbeddedId
+	private BelongingPK id = new BelongingPK();
+	
+	
+	private Integer position;
+	
+	public Belonging() {
+		
+	}
+
+	public Belonging(Game game, GameList gameList, Integer position) {
+		id.setGame(game);
+		id.setList(gameList);
+		this.position = position;
+	}
+
+	public BelongingPK getId() {
+		return id;
+	}
+
+	public void setId(BelongingPK id) {
+		this.id = id;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	public Belonging(BelongingPK id) {
+		super();
+		this.id = id;
+	}
+	
+	
+	
+	
+	
+}
